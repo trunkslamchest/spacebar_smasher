d6c97b737cb4670468937879aeaa2d72feb7b1b8
trunkslamchest <trunk9slamchest@gmail.com>
2020-07-21 11:16:48 -0400

    fixed lodash security vulnerability

--- src/utility/paths.js
+++ src/utility/paths.js
@@ -1,13 +1,16 @@
 export const fetch = {
-  // get: 'http://localhost:5001/spacebarsmasher-96ba1/us-east1/players',
-  get: 'https://us-east1-spacebarsmasher-96ba1.cloudfunctions.net/players',
-  // post: 'http://localhost:5001/spacebarsmasher-96ba1/us-east1/addScore'
-  post: 'https://us-east1-spacebarsmasher-96ba1.cloudfunctions.net/addScore'
+  get: 'http://localhost:5001/spacebarsmasher-96ba1/us-east1/players',
+  // get: 'https://us-east1-spacebarsmasher-96ba1.cloudfunctions.net/players',
+  post: 'http://localhost:5001/spacebarsmasher-96ba1/us-east1/addScore'
+  // post: 'https://us-east1-spacebarsmasher-96ba1.cloudfunctions.net/addScore'
 }
 
 export const routes = {
   root: '/',
-  home: '/spacebarsmasher',
-  game: '/spacebarsmasher/game',
-  scoreboard: '/spacebarsmasher/scoreboard'
+  // home: '/spacebarsmasher',
+  home: '/testSub',
+  // game: '/spacebarsmasher/game',
+  game: '/testSub/game',
+  // scoreboard: '/spacebarsmasher/scoreboard'
+  scoreboard: '/testSub/scoreboard'
 }
