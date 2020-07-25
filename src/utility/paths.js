8f98056a4121e7fa8936dfcd70f8b38f9e5733f4
trunkslamchest <trunk9slamchest@gmail.com>
2020-07-25 16:41:08 -0400

    built new deployable version 0.37.6

--- src/utility/paths.js
+++ src/utility/paths.js
@@ -7,14 +7,14 @@ export const fetch = {
 
 export const routes = {
   root: '/',
-  // home: '/spacebarsmasher',
-  home: '/testSub',
-  // game: '/spacebarsmasher/game',
-  game: '/testSub/game',
-  // countdown: '/spacebarsmasher/countdown',
-  countdown: '/testSub/countdown',
-  // submitScore: '/spacebarsmasher/submit_score',
-  submitScore: '/testSub/submit_score',
-  // scoreboard: '/spacebarsmasher/scoreboard'
-  scoreboard: '/testSub/scoreboard'
+  home: '/spacebarsmasher',
+  // home: '/testSub',
+  game: '/spacebarsmasher/game',
+  // game: '/testSub/game',
+  countdown: '/spacebarsmasher/countdown',
+  // countdown: '/testSub/countdown',
+  submitScore: '/spacebarsmasher/submit_score',
+  // submitScore: '/testSub/submit_score',
+  scoreboard: '/spacebarsmasher/scoreboard'
+  // scoreboard: '/testSub/scoreboard'
 }
